39b0fb9ffc4197a774210ad405ea65d585b11351
Thomas Vidori <thomas.vidori@protonmail.com>
2024-01-16 14:23:56 +0100

    Add Array types to import

--- pysyphon/postgresql/__init__.py
+++ pysyphon/postgresql/__init__.py
@@ -1,3 +1,7 @@
 import pysyphon.postgresql.postgresql_functions
 import pysyphon.postgresql.abstract_table
 from pysyphon.postgresql.abstract_table import AbstractTable
+from pysyphon.postgresql.postgresql_functions import (
+    IntArray,
+    FloatArray
+)
