b012a5285cef87aed624b59470e38ae1231b1cf4
Thomas Vidori <thomas.vidori@protonmail.com>
2024-03-21 10:21:40 +0100

    Add varchar arrays

--- pysyphon/postgresql/postgresql_functions.py
+++ pysyphon/postgresql/postgresql_functions.py
@@ -41,6 +41,24 @@ class FloatArray(list):
         return "ARRAY[]::real[]"
 
 
+class VarcharArray(list):
+    def __init__(self, list_):
+        if not isinstance(list_, typing.Iterable) & pd.isnull(list_):
+            super().__init__([])
+        else:
+            super().__init__(list_)
+
+    def to_sql_value(self) -> str:
+        if len(self) == 0:
+            return self.empty_value()
+        else:
+            return f"ARRAY{self}"
+
+    @staticmethod
+    def empty_value() -> str:
+        return "ARRAY[]::varchar[]"
+
+
 def get_connection(
         host: str,
         database: str,
